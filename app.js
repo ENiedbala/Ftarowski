b26164d16f94aca3f95919d437acd550085dc74f
Ewa Niedbała <eniedbala20@gmail.com>
2023-05-17 14:36:55 +0200

    change manu

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-// menu active
+// menu active to change size
 const hamburger = document.querySelector('.hamburger');
 const menu_list = document.querySelector('.menu_list');
 const handleClick = () => {
 	hamburger.classList.toggle('hamburger--active');
 	menu_list.classList.toggle('menu_list--active');
 };
-
 hamburger.addEventListener('click', handleClick);
