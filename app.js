08b81ea44352f325850fc3d4f460a268c777eb5f
Ewa Niedbała <eniedbala20@gmail.com>
2023-10-18 11:21:39 +0200

    resize photo

--- app.js
+++ app.js
@@ -25,49 +25,18 @@ function showSlide() {
 showSlide();
 setInterval(showSlide, 35500);
 
-// (async () =>
-// 	// async IIFE code for slider.
-// 	{
-// 		const interval = 1500, // ms
-// 			paddingRight = 10,
-// 			slideContainer = document.querySelector('.wrapper'),
-// 			slidesWrapper = document.querySelector('.carousellWrapper'),
-// 			slides = document.querySelectorAll('.carousellSlide'),
-// 			delay = ms => new Promise(r => setTimeout(r, ms)),
-// 			movLeft = (el, mov) =>
-// 				new Promise(r => {
-// 					el.ontransitionend = _ => {
-// 						el.ontransitionend = null;
-// 						el.style.transition = 'none';
-// 						r();
-// 					};
-// 					el.style.transition = '2s';
-// 					el.style.transform = `translateX(${-mov}px)`;
-// 				});
+// Menu photo
 
-// 		let index = 0;
+const Position = document.querySelectorAll('.menuPosition');
+const Photo = document.querySelectorAll('.Photo');
 
-// 		while (true) {
-// 			// infinite carrousel loop
-// 			await delay(interval);
-// 			await movLeft(slidesWrapper, slides[index].clientWidth + paddingRight);
-
-// 			slidesWrapper.appendChild(slides[index]); // mov first slide to the end
-// 			slidesWrapper.style.transform = `translateX(0)`; // rest translateX
-// 			index = ++index % slides.length;
-// 		}
-// 	})();
-
-// const Position = document.querySelectorAll('.menuPosition');
-// const Photo = document.querySelectorAll('.Photo');
-
-// for (let i = 0; i < Position.length; i++) {
-// 	Position[i].addEventListener('click', e => {
-// 		for (let a = 0; a < Photo.length; a++) {
-// 			Photo[i].classList.toggle('Photoactive');
-// 		}
-// 	});
-// }
+for (let i = 0; i < Position.length; i++) {
+	Position[i].addEventListener('click', e => {
+		for (let a = 0; a < Photo.length; a++) {
+			Photo[i].classList.toggle('Photoactive');
+		}
+	});
+}
 
 // RestaurantGallery
 const galleryImages = document.querySelectorAll('.photoGallery img');
